a57209eb07d0871ec5a0bb237af19d15462f4b95
Frank Warmerdam <warmerdam@pobox.com>
2000-11-22 16:30:28 +0000

    New

---
+++ scripts/setup.sh
@@ -0,0 +1,13 @@
+#
+# This script is intended to be sourced by sub-scripts to setup
+# common information.
+
+# Name to use for ogdi_info program.  
+
+OGDI_INFO=ogdi_info
+export OGDI_INFO
+
+# Absolute path to test_suite/data directory.
+
+TEST_DATA=/u/iii/test_suite/data
+export TEST_DATA
