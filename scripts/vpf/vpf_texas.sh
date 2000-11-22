6bf9aa33a715cc979d5161133addde1d99f9ea22
Frank Warmerdam <warmerdam@pobox.com>
2000-11-22 16:29:48 +0000

    generalized to use ../setup.sh, and ../test_comp.sh

--- scripts/vpf/vpf_texas.sh
+++ scripts/vpf/vpf_texas.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-TEXASH_URL=gltp:/vrf/u/iii/test_suite/data/vpf/vm2alv2/texash
+. ../setup.sh
+
+TEXASH_URL=gltp:/vrf$TEST_DATA/vpf/vm2alv2/texash
 
 rm -f temp_texas
 
@@ -10,31 +12,31 @@ rm -f temp_texas
 #
 # http://sourceforge.net/bugs/?func=detailbug&bug_id=123132&group_id=11181
 
-ogdi_info -u $TEXASH_URL -f Area -l 'contourl@elev(*)' -dl > temp_texas
+$OGDI_INFO -u $TEXASH_URL -f Area -l 'contourl@elev(*)' -dl > temp_texas
 
-./test_comp.sh temp_texas texas-1
+../test_comp.sh temp_texas texas-1
 
 # ----------------------------------------------------------------------------
 # Test lines and that double quoted query values work properly.
 # http://sourceforge.net/bugs/?func=detailbug&bug_id=122597&group_id=11181
 
-ogdi_info -no-dict -u $TEXASH_URL -r 31.25 31.20 -97.5 -97.55 -f Line -l 'contourl@elev(hqc="1")' -dl > temp_texas
+$OGDI_INFO -no-dict -u $TEXASH_URL -r 31.25 31.20 -97.5 -97.55 -f Line -l 'contourl@elev(hqc="1")' -dl > temp_texas
 
-./test_comp.sh temp_texas texas-2
+../test_comp.sh temp_texas texas-2
 
 # ----------------------------------------------------------------------------
 # Test Points.
 
-ogdi_info -no-dict -u $TEXASH_URL -f Point -l 'elevp@elev(*)' -dl > temp_texas
+$OGDI_INFO -no-dict -u $TEXASH_URL -f Point -l 'elevp@elev(*)' -dl > temp_texas
 
-./test_comp.sh temp_texas texas-3
+../test_comp.sh temp_texas texas-3
 
 # ----------------------------------------------------------------------------
 # Test Areas. 
 
-ogdi_info -no-dict -u $TEXASH_URL -f Area -l 'disposea@ind(*)' -dl > temp_texas
+$OGDI_INFO -no-dict -u $TEXASH_URL -f Area -l 'disposea@ind(*)' -dl >temp_texas
 
-./test_comp.sh temp_texas texas-4
+../test_comp.sh temp_texas texas-4
 
 
 
