b4875f6030176709afb33a818c183eb66f292308
Frank Warmerdam <warmerdam@pobox.com>
2000-11-22 20:51:13 +0000

    fixed ogdi_info.tst name

--- scripts/full_test.sh
+++ scripts/full_test.sh
@@ -15,11 +15,11 @@ if test ! -s ogdi_info.tst ; then
   echo "Executing OGDI_INFO=$OGDI_INFO seems to have failed."
   echo "Perhaps it is not set properly in the "
   echo `pwd`"/setup.sh script."
-  rm -f ogdi_info.txt
+  rm -f ogdi_info.tst
   exit 1
 fi
 
-rm -f ogdi_info.txt
+rm -f ogdi_info.tst
 
 # -----------------------------------------------------------------------------
 
