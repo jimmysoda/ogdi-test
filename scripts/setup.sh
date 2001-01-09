d28279a964113a1db89c6ff0f5ee442d06011d27
Frank Warmerdam <warmerdam@pobox.com>
2001-01-09 21:36:06 +0000

    changed to support OGDI_HOST

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,5 +9,9 @@ export OGDI_INFO
 
 # Absolute path to test_suite/data directory.
 
-TEST_DATA=/u/iii/test_suite/data
+TEST_DATA=/D:/warmerda/iii/test_suite/data
 export TEST_DATA
+
+# Name of host with server, may be left blank for local operation
+OGDI_HOST=
+export OGDI_HOST
