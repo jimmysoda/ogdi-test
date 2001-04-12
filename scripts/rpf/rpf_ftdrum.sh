c9b74333d971c1b12bd0b53a5adad5f2ab7991e3
Frank Warmerdam <warmerdam@pobox.com>
2001-04-12 19:18:18 +0000

    added Image test

--- scripts/rpf/rpf_ftdrum.sh
+++ scripts/rpf/rpf_ftdrum.sh
@@ -7,13 +7,17 @@ GLTP=gltp:/rpf$TEST_DATA/rpf/ftdrum/rpf
 rm -f info.log
 
 # ----------------------------------------------------------------------------
-# Capture view of DTED file.
+# Matrix view of RPF file.
 
 $OGDI_INFO -u $GLTP -f Matrix -l '10M@2@CIB@DMAAC@0' -sf 100 -dl > info.log
 
 ../test_comp.sh info.log rpf-ftdrum-1
 
 
+# ----------------------------------------------------------------------------
+# Image view of DTED file.
 
+$OGDI_INFO -u $GLTP -f Image -l '10M@2@CIB@DMAAC@0' -sf 100 -dl > info.log
 
+../test_comp.sh info.log rpf-ftdrum-2
 
