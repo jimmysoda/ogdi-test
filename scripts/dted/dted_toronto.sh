aa3d76138c1ea7603a7c5bfb5d522eac530398d3
Frank Warmerdam <warmerdam@pobox.com>
2001-04-12 13:49:09 +0000

    added capture of capabilities documents

--- scripts/dted/dted_toronto.sh
+++ scripts/dted/dted_toronto.sh
@@ -7,9 +7,9 @@ GLTP=gltp:$OGDI_HOST/dted$TEST_DATA/dted/toronto/dted
 rm -f info.log
 
 # ----------------------------------------------------------------------------
-# Capture view of DTED file.
+# Capture view of DTED file, and capabilities documents.
 
-$OGDI_INFO -u $GLTP -f Matrix -l 'DTED(DISK)' -sf 8 -dl > info.log
+$OGDI_INFO -u $GLTP -dict ogdi_server_capabilities -dict ogdi_capabilities -f Matrix -l 'DTED(DISK)' -sf 8 -dl > info.log
 
 ../test_comp.sh info.log dted-toronto-1
 
